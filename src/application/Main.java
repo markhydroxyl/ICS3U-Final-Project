825461cfbf21fad3fab9d33658973adc8b00f044
Felix Yu <felixyu2001@gmail.com>
2017-05-06 12:28:48 -0400

    Changed hard-coded values to global ints

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,22 +8,27 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 
-
 public class Main extends Application {
+	
+	final int playerWidth = 50;
+	final int screenWidth = 400;
+	final int screenHeight = 800;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Canvas playerCanvas = new Canvas(10,10);
-			playerCanvas.setTranslateX(195);
-			playerCanvas.setTranslateY(390);
+			Canvas playerCanvas = new Canvas(playerWidth,playerWidth);
 			GraphicsContext playerGC = playerCanvas.getGraphicsContext2D();
 			drawPlayerRect(playerGC);
+			playerCanvas.setTranslateX((screenWidth-(playerWidth))/2);
+			playerCanvas.setTranslateY(screenHeight-(playerWidth));
 			root.getChildren().add(playerCanvas);
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,screenWidth,screenHeight);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -31,14 +36,11 @@ public class Main extends Application {
 	
 	public void drawPlayerRect(GraphicsContext gc) {
 		gc.setFill(Color.BLACK);
-		gc.setStroke(Color.RED);
-		gc.setLineWidth(5);
-		gc.strokeLine(0, 0, 0, 10);
-		gc.strokeLine(0, 10, 10, 10);
-		gc.strokeLine(10, 10, 10, 0);
-		gc.strokeLine(10, 0, 0, 0);
-		gc.fillRect(0, 0, 10, 10);
+		gc.fillRect(0, 0, playerWidth, playerWidth);
 	}
+	
+	//Player square starts on bottom line, in the middle
+	//
 	
 	public static void main(String[] args) {
 		launch(args);
